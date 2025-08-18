fac036bcaf9293996fd56b5ab0f93382970e4b39
Code-Themba <Siyethemba3541@gmail.com>
2025-08-18 19:43:36 +0200

    Third Commit:18-08-2025-19:42: Customized Django's User Model and added my own fields

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,36 @@
+from datetime.datetime import timezone
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create(self, username, email, password=None):
+        if not email:
+            raise ValueError("Please enter a valid email address.")
+        
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self.db)
+        return user
+    
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(unique=True, max_length=50, blank=False)
+    email = models.EmailField(max_length=255, unique=True, blank=False)
+    password = models.CharField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now_add=True,default=timezone.now)
